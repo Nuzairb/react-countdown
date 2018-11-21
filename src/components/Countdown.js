0fb693e44b55d0009cc3cf54cc120284476cdbb7
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-21 20:54:38 +0500

    init

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Countdown = (props) =>
-  <section className="hero is-success is-fullheight">
-    <div className="hero-body">
-      <div className="container">
-        <h1 className="title">
-          Fullheight title
-        </h1>
-        <h2 className="subtitle">
-          Fullheight subtitle
-        </h2>
-      </div>
-    </div>
-  </section>
-export default Countdown
+export default class Countdown extends Component {
+  render() {
+    return (
+      <section className="hero is-info is-bold is-fullheight has-text-centered">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              New Year is coming Up!
+            </h1>
+            <div className="section">
+              <nav className="level">
+                <div className="level-item has-text-centered">
+                  <div>
+                    <p className="heading">Days</p>
+                    <p className="title">3,456</p>
+                  </div>
+                </div>
+                <div className="level-item has-text-centered">
+                  <div>
+                    <p className="heading">Hours</p>
+                    <p className="title">123</p>
+                  </div>
+                </div>
+                <div className="level-item has-text-centered">
+                  <div>
+                    <p className="heading">Minutes</p>
+                    <p className="title">456K</p>
+                  </div>
+                </div>
+                <div className="level-item has-text-centered">
+                  <div>
+                    <p className="heading">Seconds</p>
+                    <p className="title">789</p>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
